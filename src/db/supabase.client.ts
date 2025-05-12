b200f244d8c4311b1dae12bec12fc7262cd3280a
mmolek-sembot <mateusz.molek@sembot.io>
2025-05-12 21:45:43 +0200

    fix: update packages, addinf default user id config

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,5 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const DEFAULT_USER_ID = '6e02656e-1afb-4795-9726-a5738db2e785';
